169944deab7da5c22a3ab855433edbf8f60b19e6
cnmberches <cnmberches@gmail.com>
2023-06-05 16:33:07 +0800

    Update face_detection_with_mesh.py and implemented face mesh

--- face_detection_with_mesh.py
+++ face_detection_with_mesh.py
@@ -1,57 +1,127 @@
-import cv2
+import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+import pickle
+from keras_facenet import FaceNet
+import time
 import mediapipe as mp
 
-# Create a video capture object
-cap = cv2.VideoCapture(0)  # Change the index to select a different camera (if available)
+# Define a threshold value for face recognition
+threshold = 0.7
 
-# Define the colormap
-cmap = cv2.COLORMAP_INFERNO
-
-# Initialize mediapipe modules
+# Initialize the MediaPipe FaceMesh
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
+# Load the saved SVM model
+with open("svm_model_160x160.pkl", 'rb') as f:
+    model = pickle.load(f)
+
+# Read the pre-trained face embedding model
+facenet = FaceNet()
+
+# Load the saved face embeddings and labels
+faces_embeddings = np.load("faces_embeddings_done_4classes.npz")
+X, Y = faces_embeddings['arr_0'], faces_embeddings['arr_1']
+encoder = LabelEncoder()
+encoder.fit(Y)
+
+# Read the Haar cascade classifier for face detection
+haarcascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# Open the camera and start capturing video
+cap = cv.VideoCapture(0)
+
+save_path = 'captured_img'  # Replace with your desired save folder path
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+num_screenshots = 0
+
 with mp_face_mesh.FaceMesh() as face_mesh:
     while True:
-        # Read a frame from the video capture
-        ret, frame = cap.read()
 
-        # Convert the frame to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #while cap.isOpened():
+        # Read a frame from the camera
+        _, frame = cap.read()
 
-        # Perform your computer vision analysis or processing on the grayscale frame
-        # For example, let's detect faces using Haar cascades
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        # Convert the image color to RGB
+        rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        # Convert the RGB into gray since haarcascade requires gray images
+        gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # Detect faces using the Haar cascade classifier
+        faces = haarcascade.detectMultiScale(gray_img, 1.3, 5)
 
         # Convert the frame to RGB format for face mesh detection
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
         # Detect face landmarks and draw face mesh
         results = face_mesh.process(rgb_frame)
         face_landmarks = results.multi_face_landmarks
 
-        # Draw face mesh on the frame
-        if face_landmarks:
-            for face_landmark in face_landmarks:
-                # Draw connections
-                mp_drawing.draw_landmarks(frame, face_landmark, mp_face_mesh.FACEMESH_TESSELATION,
-                                          landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=0, circle_radius=0))
+        # Process each detected face
+        for x, y, w, h in faces:
+            # Extract the face region from the image
+            img = rgb_img[y:y + h, x:x + w]
 
-                # Draw filled polygons for each face mesh triangle
-                for triangle in mp_face_mesh.FACEMESH_TESSELATION:
-                    triangle_points = [(face_landmark.landmark[index].x * frame.shape[1],
-                                        face_landmark.landmark[index].y * frame.shape[0]) for index in triangle]
+            # Resize the face region to 160x160
+            img = cv.resize(img, (160, 160))
 
-        # Display the frame with face mesh
-        cv2.imshow('Thermal Face', frame)
+            # Convert the 3D image array to a 4D array of shape (1, 160, 160, 3)
+            img = np.expand_dims(img, axis=0)
 
-        # Exit the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # Embed the face using FaceNet
+            embedding = facenet.embeddings(img)
+
+            # Classify the face using the SVM model
+            ypreds = model.predict_proba(embedding)
+            ypreds_max = np.max(ypreds)
+            ypreds_label = np.argmax(ypreds)
+            if ypreds_max > threshold:
+                face_name = encoder.inverse_transform([ypreds_label])[0]
+                # Draw a rectangle around the face and display the name
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+                cv.putText(frame, face_name, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv.LINE_AA)
+            else:
+                face_name = "not registered"
+                # Draw a rectangle around the face and display the name
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+                cv.putText(frame, face_name, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv.LINE_AA)
+
+            # Draw face mesh on the frame
+            if face_landmarks:
+                for face_landmark in face_landmarks:
+                    # Draw connections
+                    mp_drawing.draw_landmarks(frame, face_landmark, mp_face_mesh.FACEMESH_TESSELATION,
+                                              landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=0, circle_radius=0))
+
+                    # Draw filled polygons for each face mesh triangle
+                    for triangle in mp_face_mesh.FACEMESH_TESSELATION:
+                        triangle_points = [(face_landmark.landmark[index].x * frame.shape[1],
+                                            face_landmark.landmark[index].y * frame.shape[0]) for index in triangle]
+
+        # Display the result on the screen
+        cv.imshow("Face Recognition:", frame)
+
+        # Save a screenshot if the number of screenshots is less than 3
+        if num_screenshots < 3:
+            screenshot_name = 'screenshot_{}.png'.format(num_screenshots)
+            cv.imwrite(os.path.join(save_path, screenshot_name), frame)
+            num_screenshots += 1
+
+            # Delay for 1 second before capturing the next screenshot
+            time.sleep(1)
+
+        # Exit the loop if the 'q' key is pressed
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
 # Release the video capture object and close all windows
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
