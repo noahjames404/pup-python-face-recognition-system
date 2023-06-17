412a95f21da75d28fd744cdefe7040f70d077e63
LAPTOP-STTLPSP7\yanga <yanganoah404@gmail.com>
2023-06-18 02:49:26 +0800

    partial:layout revamp

--- main_gui.py
+++ main_gui.py
@@ -1,46 +1,82 @@
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import PanedWindow, Label, font
 
-root=Tk()
-root.title("Pythong Guides")
-root.geometry("500x400")
+app_name = "Face Recognition System"
+app_intro = "Welcome Back!"
+app_guidelines = "Please stand still in front of the camera"
 
-def AttachLabel(panel:PanedWindow, label:StringVar) -> Label:
-     l = Label(panel,text=label)
-     panel.add(l)
-     return l
+frame_size = "500x400" 
+color_bg = "#024BAA"
+color_accent = "#004AAF"
+color_fg = "#FCFDFC" 
 
-def AttachPanel(container:PanedWindow, bg="red") -> PanedWindow:
-     panel = PanedWindow(bg=bg)
-     container.add(panel)
-     return panel
+font_family = "Verdana"
+font_size = 12
+font_style = "normal"
 
-pmain = PanedWindow(orient=VERTICAL)
-pmain.pack(fill=BOTH)
+class GUIBuilder:
+    def AttachLabel(self,panel:PanedWindow, label:StringVar, bgc=color_bg,fgc=color_fg) -> Label:
+        l = Label(panel,text=label,bg=bgc,fg=fgc,font=self.Font())
+        panel.add(l)
+        return l
 
-pheading = AttachPanel(pmain) 
+    def AttachPanel(self,container:PanedWindow, bgc="red") -> PanedWindow:
+        panel = PanedWindow(bg=bgc)
+        container.add(panel)
+        return panel
 
-pheading["orient"] = VERTICAL 
-AttachLabel(pheading, "HEADING") 
-AttachLabel(pheading, "HEADING") 
-AttachLabel(pheading, "HEADING") 
+    def Build(self):
+        root=Tk()
+        root.title(app_name)
+        root.geometry(frame_size)   
+
+        pmain = PanedWindow(orient=VERTICAL,bg=color_bg)
+        pmain.pack(fill=BOTH)
+
+        self.Header(pmain)
+        self.Body(pmain) 
+        
+        root.mainloop();
+    
+    def Font(self,fsize=font_size,fweight=font_style) -> font.Font:
+        return font.Font(family=font_family, size=fsize, weight=fweight)
+
+    def Header(self,container:PanedWindow) -> PanedWindow :
+        panel = self.AttachPanel(container,bgc=color_bg)
+        self.AttachLabel(panel, app_name) 
+        self.AttachLabel(panel, app_intro) 
+        self.AttachLabel(panel, app_guidelines) 
+        panel["orient"] = VERTICAL 
+
+    def Body(self,container:PanedWindow) -> PanedWindow :
+        panel = self.AttachPanel(container,bgc=color_bg) 
+        panel["orient"] = HORIZONTAL
+        panel.pack(fill=BOTH) 
+
+        pcam = self.AttachPanel(panel,bgc=color_bg) 
+        pcam.pack(side=LEFT, fill=BOTH, expand=1)
+        pcam_label = self.AttachLabel(pcam,"PCAM") 
+
+        pdetails = self.AttachPanel(panel,bgc=color_bg) 
+        pdetails["orient"] = VERTICAL
+        pdetails.pack(side=LEFT, expand=1)
+        pdetails_label = self.AttachLabel(pdetails,"Stand By") 
+        pdetails_label = self.AttachLabel(pdetails,"Henry Sy.") 
+        pdetails_label = self.AttachLabel(pdetails,"0935-380-2346") 
+        pdetails_label = self.AttachLabel(pdetails,"grade & section") 
+        pdetails_label = self.AttachLabel(pdetails,"4-Archimedes") 
+        pdetails_label = self.AttachLabel(pdetails,"guardian") 
+        pdetails_label = self.AttachLabel(pdetails,"rose marie") 
+ 
+
+gui = GUIBuilder()
+gui.Build();
 
 
-pbody = AttachPanel(pmain, bg="green") 
-pbody["orient"] = HORIZONTAL
-pbody.pack(fill=BOTH) 
 
-pcam = AttachPanel(pbody) 
-pcam.pack(side=LEFT, fill=BOTH, expand=1)
-pcam_label = AttachLabel(pcam,"PCAM") 
 
-pdetails = AttachPanel(pbody) 
-pdetails.pack(side=LEFT, fill=BOTH, expand=1)
-pdetails_label = AttachLabel(pdetails,"details")
 
-pbody.pack_propagate(0)
-pbody.paneconfig(pcam,weight=70)
 
-root.mainloop();
 
